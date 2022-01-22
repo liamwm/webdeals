5633a5204fd466f7a2c4f0f03f9cbf5b0586048a
liamwm <liam.wm1@gmail.com>
2022-01-22 16:14:32 +1100

    clean up fetch product

--- fetch.js
+++ fetch.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer")
 
-const MAX_PRODS = 20
+const MAX_PRODS = 5
 
 const getStuff = async (req, res) => {
   const browser = await puppeteer.launch({headless: false});
@@ -10,7 +10,7 @@ const getStuff = async (req, res) => {
   // const products = await fetchProducts(page, url)
 
   //const url = "https://www.jbhifi.com.au/sitemap_collections_1.xml"
-  const url = "https://www.jbhifi.com.au/sitemap_products_1.xml?from=1614257324130&to=1618967593058"
+  const url = "https://www.jbhifi.com.au/sitemap_products_1.xml?from=1614257324130&to=1618968379490"
   const products = await crawlSite(page, url)
 
   res.send(products)
@@ -30,10 +30,14 @@ const crawlSite = async (page, url) => {
     urls.push(pageUrl)
   }
 
-  for (const url of urls.filter(url => !urlIgnore.includes(url))) {
+  //for (const url of urls.filter(url => !urlIgnore.includes(url))) {
+  i = 0
+  const toCrawl = urls.filter(url => !urlIgnore.includes(url))
+  while (products.length < MAX_PRODS) {
     //const new_prods = await fetchProducts(page, url)
-    const new_prods = await fetchProduct(page, url)
+    const new_prods = await fetchProduct(page, toCrawl[i++])
     if (new_prods) {products.push(new_prods)}
+    console.log("")
   }
   return products
 }
@@ -43,71 +47,32 @@ const fetchProduct = async (page, url) => {
   console.log(url)
 
   const nameSelector = "h1[itemprop=name]"
-  const pricetagSelector = "meta[itemprop=price]"
+  const priceSelector = "meta[itemprop=price]"
+  const imgSelector = "img"
 
-  await page.waitForSelector(nameSelector).catch((err) => {
-    console.log("Name not found")
-  })
-  const name = await page.$eval(nameSelector, (el)=>el.innerHTML).catch((err) => {
-    console.log("No product found")
-  })
-  if (name) {console.log(name)}
+  const name = await fetchAttribute(page, nameSelector, (el)=>el.innerHTML)
+  const price = await fetchAttribute(page, priceSelector, (el)=>el.content)
+  const img = await fetchAttribute(page, imgSelector, (el)=>el.src)
 
-  await page.waitForSelector(pricetagSelector).catch((err) => {
-    console.log("Price not found")
-  })
-  const pricetag = await page.$eval(pricetagSelector, (el)=>el.content).catch((err) => {
-    console.log("No product found")
-  })
-  if (pricetag) {console.log(pricetag)}
-  return {name, pricetag}
-}
-
-const fetchProducts = async (page, url) => {
-  await page.goto(url);
-  console.log(url)
-  products = []
-
-  const container_selector = "div.collection-results-loop > div"//"#collection-container > div:nth-child(5) > div.collection-results-loop > div"
-  await page.waitForSelector(container_selector)
-  const product_container = await page.$(container_selector)
-  const productsHandle = await page.$$(container_selector + " > *")
-  console.log("Products found: " + productsHandle.length)
-
-  for (const prod of productsHandle) {
-    const nameSelector = "div > a > div > div.ais-hit--details.product-tile__details > h4"
-    const nameHandle = await prod.$(nameSelector)
-    const name = await page.evaluate((nameEl) => nameEl.innerHTML, nameHandle)
-
-    const priceSelector = "div > div.ais-hit--cart.product-tile__cta > a > div > div > span"
-    const priceHandle = await prod.$(priceSelector)
-    const priceHTML = await page.evaluate(
-      (priceEl) => priceEl.innerHTML, priceHandle
-    )
-    const priceSearch = priceHTML.match(/\d+/g)
-    const price = (priceSearch != []) ? priceSearch[0] : null
-
-    const linkSelector = "a"
-    const linkHandle = await prod.$(linkSelector)
-    const link = await page.evaluate((linkEl) => {
-      return linkEl.attributes.getNamedItem("href").value
-    }, linkHandle)
-
-    const imgSelector = "img"
-    const imgHandle = await prod.$(imgSelector)
-    const img = await page.evaluate((imgEl) => {
-      return imgEl.attributes.getNamedItem("src").value
-    }, imgHandle)
-
-    //console.log(name, price, link, img)
-    products.push({"name": name, "price": price, "link": link, "img": img})
-  }
-
-  return products
+  return {"name": name, "price": price, "img": img, "url": url}
 }
 
 const removeTags = (htmlStr) => {
   return htmlStr.replace(/<[\s\S]+?>/g, "")
+}
+
+const fetchAttribute = async (page, selector, pageFunction) => {
+  // Wait for page to load desired element
+  await page.waitForSelector(selector).catch((err) => {
+    console.log("Selector " + selector + " not found")
+  })
+  // Retrieve the desired info from element
+  const attr = await page.$eval(selector, pageFunction).catch((err) => {
+    console.log("No value retrieved for selector " + selector)
+  })
+
+  if (attr) {console.log(attr)}
+  return attr
 }
 
 

88ce939f69a3e54d209f6e8a5c22426d2ed0c685
liamwm <liam.wm1@gmail.com>
2022-02-17 15:57:00 +1100

    change port to use env variable

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const fetch = require("./fetch.js")
 
 
 const app = express()
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.set("view engine", "pug")
 app.get("/", async (req, res) => {
